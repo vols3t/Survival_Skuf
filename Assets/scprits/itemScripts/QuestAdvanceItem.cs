cc60f292f7dc4170823750ce8fb51b1edfc0cca2
Stepa_Volkov <wdgetso2@gmail.com>
2025-05-05 23:58:02 +0500

    teleport after item

--- Assets/scprits/itemScripts/QuestAdvanceItem.cs
+++ Assets/scprits/itemScripts/QuestAdvanceItem.cs
@@ -1,18 +1,20 @@
 using UnityEngine;
-
-[CreateAssetMenu(menuName = "Inventory/Quest Advance Item")]
 public class QuestAdvanceItem : InventoryItem
 {
-    public GameObject triggerToActivate; 
+    public string targetObjectName; 
+
     public override void Use()
-    
     {
         base.Use();
 
-        if (triggerToActivate != null)
-            triggerToActivate.SetActive(true);
-        
-        if (QuestManager.Instance != null)
-            QuestManager.Instance.TriggerNextQuest();
+        GameObject obj = GameObject.Find(targetObjectName);
+        if (obj != null)
+        {
+            var teleportScript = obj.GetComponent<TeleportOnTrigger>();
+            if (teleportScript != null)
+                teleportScript.SetActive(); 
+        }
+
+        QuestManager.Instance?.TriggerNextQuest();
     }
 }
