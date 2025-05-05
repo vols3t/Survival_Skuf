883b196cfd19d14d255b6ef9d5cc3e383e8e08cb
Vladislav Volkov <vladqwe111@yandex.ru>
2025-05-06 03:12:25 +0500

    Update Message

--- Assets/scprits/StartDialog.cs
+++ Assets/scprits/StartDialog.cs
@@ -1,22 +1,33 @@
-using System;
 using UnityEngine;
 using DialogueEditor;
 
-public class StartDialog : MonoBehaviour
+public class SequentialDialogTrigger : MonoBehaviour
 {
-    [SerializeField] private NPCConversation myConversation;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private NPCConversation conversation;
+    [SerializeField] private GameObject nextTrigger; // Следующий триггер (если есть)
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && ConversationManager.Instance != null)
         {
-            if (ConversationManager.Instance != null && myConversation != null)
-            {
-                ConversationManager.Instance.StartConversation(myConversation);
-                GetComponent<Collider2D>().enabled = false; // Отключаем триггер
-            }
+            // Запускаем диалог и отключаем текущий триггер
+            ConversationManager.Instance.StartConversation(conversation);
+            GetComponent<Collider2D>().enabled = false;
+
+            // Подписываемся на событие завершения диалога
+            ConversationManager.OnConversationEnded += EnableNextTrigger;
         }
+    }
+
+    private void EnableNextTrigger()
+    {
+        // Включаем следующий триггер (если он есть)
+        if (nextTrigger != null)
+        {
+            nextTrigger.GetComponent<Collider2D>().enabled = true;
+        }
+
+        // Отписываемся от события, чтобы не вызывалось повторно
+        ConversationManager.OnConversationEnded -= EnableNextTrigger;
     }
 }
