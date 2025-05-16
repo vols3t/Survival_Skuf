88f4b71de14df6cd538eba8c2119ab809db7c326
Vladislav Volkov <vladqwe111@yandex.ru>
2025-05-16 13:51:28 +0500

    + телепорт со страшной на кухню страшную и обратно

--- Assets/scprits/Quest/QuestPanelController.cs
+++ Assets/scprits/Quest/QuestPanelController.cs
@@ -4,11 +4,19 @@ using UnityEngine.UI;
 
 public class QuestPanelController : MonoBehaviour
 {
+    [Header("Настройки квеста")]
     public TMP_InputField inputField;
     public TMP_Text feedbackText;
     public Button confirmButton;
     public string correctAnswer = "unity";
+    
+    [Header("Объекты для управления")]
+    [Tooltip("Объекты, которые нужно выключить при правильном ответе")]
     public GameObject[] objectsToDisable;
+    
+    [Tooltip("Необязательно: объект, который нужно включить при правильном ответе")]
+    public GameObject objectToEnable;     
+    
     public movee playerMovement;
 
     private void Start()
@@ -16,6 +24,12 @@ public class QuestPanelController : MonoBehaviour
         gameObject.SetActive(false);
         feedbackText.gameObject.SetActive(false);
         confirmButton.onClick.AddListener(CheckAnswer);
+        
+        // Выключаем объект при старте, если он задан
+        if (objectToEnable != null)
+        {
+            objectToEnable.SetActive(false);
+        }
     }
 
     public void ShowQuest()
@@ -44,10 +58,18 @@ public class QuestPanelController : MonoBehaviour
 
         if (isCorrect)
         {
+            // Выключаем указанные объекты
             foreach (GameObject obj in objectsToDisable)
             {
                 if (obj != null) obj.SetActive(false);
             }
+            
+            // Включаем нужный объект, если он указан
+            if (objectToEnable != null)
+            {
+                objectToEnable.SetActive(true);
+            }
+            
             HideQuest();
         }
     }
